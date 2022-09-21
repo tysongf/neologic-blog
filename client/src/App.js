edf3d257954e6d816216dec2c55ffcf1bc9ea09c
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-21 00:14:19 -0700

    Added Dark Mode button.

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ function App() {
          <Navbar bg="primary" variant="dark">
             <Container>
                <Navbar.Brand href="#home">neologic</Navbar.Brand>
+               <Button variant="dark" className="justify-content-end">
+                  Dark Mode
+               </Button>
             </Container>
          </Navbar>
          <EntryList />
