b3eefe5d94aca745a32181fc9ba46c65cd1de7d7
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-22 06:35:38 -0700

    renamed store to contexts

---
+++ client/src/contexts/EntriesProvider.js
@@ -0,0 +1,49 @@
+import { useReducer } from "react";
+import EntriesContext from "./entries-context";
+
+const defaultEntriesState = {
+   entries: [],
+};
+
+const entriesReducer = (state, action) => {
+   if (action.type === "ADD_ENTRY") {
+      const updatedEntries = state.entries.concat(action.entry);
+      return {
+         entries: updatedEntries,
+      };
+   } else if (action.type === "REMOVE_ENTRY") {
+      const updatedEntries = state.entries.splice(action.entry);
+      return {
+         entries: updatedEntries,
+      };
+   }
+};
+
+const EntriesProvider = (props) => {
+   const [entriesState, dispatchEntriesAction] = useReducer(
+      entriesReducer,
+      defaultEntriesState
+   );
+
+   const addEntryHandler = (entry) => {
+      dispatchEntriesAction({ type: "ADD_ENTRY", entry: entry });
+   };
+
+   const removeEntryHandler = (id) => {
+      dispatchEntriesAction({ type: "REMOVE_ENTRY", id: id });
+   };
+
+   const entriesContext = {
+      entries: entriesState.entries,
+      addEntry: addEntryHandler,
+      removeEntry: removeEntryHandler,
+   };
+
+   return (
+      <EntriesContext.Provider value={entriesContext}>
+         {props.children}
+      </EntriesContext.Provider>
+   );
+};
+
+export default EntriesProvider;
